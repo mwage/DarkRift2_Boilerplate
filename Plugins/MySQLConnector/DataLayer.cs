44ae99ff2f5f9a41d23d848e55ed0a7ff52d20b2
Michael Wagner <michael.joh.wagner@gmail.com>
2018-08-01 12:39:42 +0200

    fixed sql implementation

--- Plugins/MySQLConnector/DataLayer.cs
+++ Plugins/MySQLConnector/DataLayer.cs
@@ -14,46 +14,45 @@ namespace MySQLConnector
         }
 
         public string Name { get; }
-
-        public IUser GetUser(string username)
+        public void GetUser(string username, Action<IUser> callback)
         {
             var row = _database.ExecuteQuery(
                 "SELECT id FROM users WHERE username = @username LIMIT 1 ",
                 new QueryParameter("username", _database.EscapeString(username)))[0];
-            return new User(Convert.ToString(row["username"]), Convert.ToString(row["password"]), null, null, null);
+            callback(new User(Convert.ToString(row["username"]), Convert.ToString(row["password"]), null, null, null));
         }
 
-        public bool UsernameAvailable(string username)
+        public void UsernameAvailable(string username, Action<bool> callback)
         {
             throw new NotImplementedException();
         }
 
-        public void AddNewUser(string username, string password)
+        public void AddNewUser(string username, string password, Action callback)
         {
             throw new NotImplementedException();
         }
 
-        public void DeleteUser(string username)
+        public void DeleteUser(string username, Action callback)
         {
             throw new NotImplementedException();
         }
 
-        public void AddRequest(string sender, string receiver)
+        public void AddRequest(string sender, string receiver, Action callback)
         {
             throw new NotImplementedException();
         }
 
-        public void RemoveRequest(string sender, string receiver)
+        public void RemoveRequest(string sender, string receiver, Action callback)
         {
             throw new NotImplementedException();
         }
 
-        public void AddFriend(string sender, string receiver)
+        public void AddFriend(string sender, string receiver, Action callback)
         {
             throw new NotImplementedException();
         }
 
-        public void RemoveFriend(string sender, string receiver)
+        public void RemoveFriend(string sender, string receiver, Action callback)
         {
             throw new NotImplementedException();
         }
